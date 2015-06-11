a0397c4854fcdb6ee6525c14a0922ee10fb4f819
Miguel Avila Monroy <soyer893@gmail.com>
2015-06-11 23:33:21 +0200

    Primera subida gh-pages

--- SC.js
+++ SC.js
@@ -1,25 +1,26 @@
+$(document).ready(function() {
 
+  SC.initialize({
+    client_id: "c202b469a633a7a5b15c9e10b5272b78",
+   redirect_uri: "http://connect.soundcloud.com/examples/callback.html"
+  });
 
-SC.initialize({
-  client_id: "c202b469a633a7a5b15c9e10b5272b78",
- redirect_uri: "http://connect.soundcloud.com/examples/callback.html"
-});
+  $("#connect").live("click", function(){
+    SC.connect(function(){
+      SC.get("/me", function(me){
+        $("#username").text(me.username);
+        $("#description").val(me.description);
+      });
+   });
+  });
 
-$("#connect").live("click", function(){
-  SC.connect(function(){
-    SC.get("/me", function(me){
-      $("#username").text(me.username);
-      $("#description").val(me.description);
+  $("#update").live("click", function(){
+    SC.put("/me", {user: {description: $("#description").val()}}, function(response, error){
+      if(error){
+        alert("Some error occured: " + error.message);
+      }else{
+        alert("Profile description updated!");
+      }
     });
- });
-});
-
-$("#update").live("click", function(){
-  SC.put("/me", {user: {description: $("#description").val()}}, function(response, error){
-    if(error){
-      alert("Some error occured: " + error.message);
-    }else{
-      alert("Profile description updated!");
-    }
   });
 });
